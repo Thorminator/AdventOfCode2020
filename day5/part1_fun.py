527fcb42a6b264af10cf78907b72ab10d3a1d4f6
Thor <thor@fambagge.dk>
2020-12-05 10:53:12 +0100

    Day 5 - more fun!

--- day5/part1_fun.py
+++ day5/part1_fun.py
@@ -1,9 +1,4 @@
 import re
 
-
-def parse(s):
-    return int(re.sub(r"[FL]", "0", re.sub(r"[BR]", "1", s)), 2)
-
-
 with open("input.txt") as f:
-    print(max([parse(s[:7]) * 8 + parse(s[7:]) for s in f.readlines()]))
+    print(max([int(re.sub(r"[FL]", "0", re.sub(r"[BR]", "1", s)), 2) for s in f.readlines()]))
--- day5/part2_fun.py
+++ day5/part2_fun.py
@@ -1,13 +1,7 @@
 import re
 
-
-def parse(s):
-    return int(re.sub(r"[FL]", "0", re.sub(r"[BR]", "1", s)), 2)
-
-
 with open("input.txt") as f:
-    ids = [parse(s[:7]) * 8 + parse(s[7:]) for s in f.readlines()]
+    ids = [int(re.sub(r"[FL]", "0", re.sub(r"[BR]", "1", s)), 2) for s in f.readlines()]
     for i in range(min(ids), max(ids)):
         if i not in ids:
             print(i)
-
