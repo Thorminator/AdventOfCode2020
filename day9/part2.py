6beb9fe5c1ad417cfca24b94ee72b92222eb4ad4
Thor <thor@fambagge.dk>
2020-12-09 10:55:41 +0100

    Day 9 correct names

--- day9/part2.py
+++ day9/part2.py
@@ -1,5 +1,6 @@
 import itertools
 
+
 def parse_input(file_name):
     return [int(n) for n in open(file_name).readlines()]
 
@@ -11,9 +12,19 @@ def is_sum_of_two(number, candidates):
     return False
 
 
-numbers = parse_input("input.txt")
-preamble_length = 25
-for i in range(preamble_length, len(numbers)):
-    if not is_sum_of_two(numbers[i], numbers[i-preamble_length: i]):
-        print(numbers[i])
-        break
+def find_contiguous_set(numbers, target):
+    added = []
+    current = 0
+    for n in numbers:
+        added.append(n)
+        current += n
+        while current > target:
+            removed = added.pop(0)
+            current -= removed
+        if current == target:
+            return added
+    return None
+
+
+contiguous_set = find_contiguous_set(numbers=parse_input("input.txt"), target=133015568)
+print(min(contiguous_set) + max(contiguous_set))
