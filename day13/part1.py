5bbefb0d588b1aacd4d6597da78618d8c74f813e
Thor <thor@fambagge.dk>
2020-12-14 14:28:13 +0100

    Small updates

--- day13/part1.py
+++ day13/part1.py
@@ -1,9 +1,11 @@
 import math
 
+
 def parse_input(file_name):
     with open(file_name) as f:
         input_split = f.read().split("\n")
         return int(input_split[0]), [int(s) for s in input_split[1].split(",") if s != "x"]
+
 
 timestamp, buses = parse_input("input.txt")
 possible_departures = [math.ceil(timestamp / bus) * bus for bus in buses]
--- day13/part2.py
+++ day13/part2.py
@@ -3,5 +3,6 @@ def parse_input(file_name):
         input_split = f.read().split("\n")
         return int(input_split[0]), [int(s) if s != "x" else None for s in input_split[1].split(",")]
 
+
 _, buses = parse_input("example1.txt")
 values = [bus + i for i, bus in enumerate(buses) if bus is not None]
