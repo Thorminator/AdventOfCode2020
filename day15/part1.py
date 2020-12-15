35d033fbf24076455285faec40eaf7f53cd0ceae
Thor <thor@fambagge.dk>
2020-12-15 08:29:37 +0100

    Day 15

---
+++ day15/part1.py
@@ -0,0 +1,10 @@
+def parse_input(file_name):
+    with open(file_name) as f:
+        return f.read().split(",")
+
+
+spoken = {int(n): turn + 1 for turn, n in enumerate(parse_input("input.txt"))}
+current = list(spoken.keys())[-1]
+for i in range(len(spoken), 2020):
+    spoken[current], current = (i, i - spoken[current]) if current in spoken else (i, 0)
+print(current)
