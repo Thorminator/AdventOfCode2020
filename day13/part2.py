b61f14ecc540c1951fae4bfc7c24991ef4a9219c
Thor <thor@fambagge.dk>
2020-12-15 11:20:27 +0100

    Day 13 part 2

--- day13/part2.py
+++ day13/part2.py
@@ -1,8 +1,32 @@
+import math
+
 def parse_input(file_name):
     with open(file_name) as f:
         input_split = f.read().split("\n")
         return int(input_split[0]), [int(s) if s != "x" else None for s in input_split[1].split(",")]
 
 
-_, buses = parse_input("example1.txt")
-values = [bus + i for i, bus in enumerate(buses) if bus is not None]
+_, buses = parse_input("input.txt")
+matched_buses = []
+t = buses[0]
+
+
+def match_bus(bus, index, iteration_value, current_t):
+    t = current_t
+    if t == 0:
+        t += iteration_value
+    while True:
+        if (t + index) % bus == 0:
+            return t
+        else:
+            t += iteration_value
+
+
+for i, bus in enumerate(buses):
+    if bus is None:
+        continue
+
+    iteration_value = math.lcm(*matched_buses) if len(matched_buses) > 0 else bus
+    t = match_bus(bus, i, iteration_value, t)
+    matched_buses.append(bus)
+print(t)
